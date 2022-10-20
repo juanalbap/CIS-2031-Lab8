7ccc362160a7ce2c52caa4988147aca11c9a8f9e
jxa00688 <jxa00688@castleton.edu>
2022-10-20 10:21:54 -0400

    Finished Battle Royale

--- MonsterFight.cpp
+++ MonsterFight.cpp
@@ -1,3 +1,6 @@
+//I edited the fight function so a fight would not take place if either of the monsters or  both of them were dead. Also, if a monster wins in one round, previously, the word //"rounds!" was output to the console. Now, if round == 1, "round!" is output to the console. Also, a winner is announced at the end.
+
+
 #include <iostream>
 #include <string>
 using std::cout;
@@ -31,7 +34,6 @@ void printMonsterStats(Monster m){
         cout << "DEAD!";
     }
 
-    cout << endl;
 
 }
 
@@ -45,15 +47,32 @@ void fight(Monster &m1, Monster &m2){
         m2.hitpoints = m2.hitpoints - m1.attack;
     }
     if ( m1.hitpoints > 0 && m2.hitpoints <= 0 ){
-        cout << m1.name << " wins in " << round << " rounds!" << endl;
+        if (round == 0){
+            cout << "No fight! " << m2.name << " is already dead!" << endl;
+        } else if (round == 1) {
+            cout << m1.name << " wins in " << round << " round!" << endl;
+        } else {
+            cout << m1.name << " wins in " << round << " rounds!" << endl;
+        }
+        
     } else if ( m1.hitpoints <= 0 && m2.hitpoints > 0 ){
-        cout << m2.name << " wins in " << round << " rounds!" << endl;
+        if (round == 0){
+            cout << "No fight! " << m1.name << " is already dead!" << endl;
+        } else if (round == 1){
+            cout << m2.name << " wins in " << round << " round!" << endl;
+        } else {
+            cout << m2.name << " wins in " << round << " rounds!" << endl;
+        }
     } else {
-        cout << "It's a tie!" << endl;
+        if (round == 0){
+            cout << "No fight! Both " << m2.name << " and " << m1.name << " are already dead!" << endl;
+        } else {
+        cout << "It's a tie in " << round << " rounds!" << endl;
     }
+  }
 }
 
-void main(){
+int main(){
 //Battle Royale
 
 for (int i = 0; i < 5; i++) {
@@ -64,12 +83,36 @@ for (int i = 0; i < 5; i++) {
     }
 }
 
-cout << "Final Results: " << endl;
+cout << endl << "Final Results: " << endl;
 printMonsterStats(monsters[0]);
-printMonsterStats(monsters[1]);
-printMonsterStats(monsters[2]);
-printMonsterStats(monsters[3]);
-printMonsterStats(monsters[4]);
+if (monsters[0].hitpoints > 0) {
+    cout << " Winner!";
+}
+cout << endl;
 
-    
+printMonsterStats(monsters[1]);
+if (monsters[1].hitpoints > 0) {
+    cout << " Winner!";
+}
+cout << endl;
+
+printMonsterStats(monsters[2]);
+if (monsters[2].hitpoints > 0) {
+    cout << " Winner!";
+}
+cout << endl;
+
+printMonsterStats(monsters[3]);
+if (monsters[3].hitpoints > 0) {
+    cout << " Winner!";
+}
+cout << endl;
+
+printMonsterStats(monsters[4]);
+if (monsters[4].hitpoints > 0) {
+    cout << " Winner!";
+}
+cout << endl;
+
+   return 0; 
 }
